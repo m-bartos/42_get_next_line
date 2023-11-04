d6456328829c1d0124702b66d7b213a5390a23d6
m-bartos <m.bartos96@seznam.cz>
2023-11-04 22:16:17 +0100

    Update: get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:05:16 by mbartos           #+#    #+#             */
-/*   Updated: 2023/11/03 14:14:36 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/04 19:52:31 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,10 @@
 # include <unistd.h>
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 5
+#  define BUFFER_SIZE 10
 # endif
 
 char	*get_next_line(int fd);
-void	ft_bzero(void *s, size_t n);
 void	*ft_calloc(size_t count, size_t size);
 char	*ft_strjoin(char const *s1, char const *s2);
 
