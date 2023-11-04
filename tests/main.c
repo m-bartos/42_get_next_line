34ea15325ac1b0290ac362635b106c4c7c81895e
m-bartos <m.bartos96@seznam.cz>
2023-11-04 22:25:31 +0100

    Tests: update main.c test file

--- tests/main.c
+++ tests/main.c
@@ -6,34 +6,43 @@
 /*   By: mbartos <mbartos@student.42prague.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/02 10:08:56 by mbartos           #+#    #+#             */
-/*   Updated: 2023/11/02 14:55:54 by mbartos          ###   ########.fr       */
+/*   Updated: 2023/11/04 19:56:32 by mbartos          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../get_next_line.h"
 
-// cc -Wall -Wextra -Werror -D BUFFER_SIZE=1 get_next_line_utils.c ./tests/main.c get_next_line.c && ./a.out
+// cc -Wall -Wextra -Werror -D BUFFER_SIZE=1 *.c ./tests/main.c && ./a.out
 
-#include<stdio.h>
+#include <stdio.h>
+
+void	print_string_fd(int fd, char *line)
+{
+	line = get_next_line(fd);
+	printf("Line: |%s|\n", line);
+	free(line);
+}
 
 int	main(void)
 {
 	int		fd;
 	char	*line;
 
+	line = NULL;
 	fd = open("test.txt", O_RDONLY);
 	if (fd == -1)
 		write(2, "Problem opening file.", 21);
-	line = get_next_line(fd);
-	printf("%s\n", line);
-	line = get_next_line(fd);
-	printf("%s\n", line);
-	line = get_next_line(fd);
-	printf("%s\n", line);
-	line = get_next_line(fd);
-	printf("%s\n", line);
-	line = get_next_line(fd);
-	printf("%s\n", line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	close(fd);
+	fd = open("test.txt", O_RDONLY);
+	if (fd == -1)
+		write(2, "Problem opening file.", 21);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
+	print_string_fd(fd, line);
 	close(fd);
 	return (0);
 }
